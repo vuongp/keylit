6a0d30caeeafa18f8543a73864ddfd96b6d844b5
Vuong Pham <h.vuongpham@gmail.com>
2026-06-27 20:47:48 +0200

    Initial Commit

---
+++ KeyLit/KeyLitApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyLitApp.swift
+//  KeyLit
+//
+//  Created by Vuong Pham on 27/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct KeyLitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
