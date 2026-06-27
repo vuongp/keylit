2053fae9f17224d6b9c780a76d24a4aa2a387054
Vuong Pham <h.vuongpham@gmail.com>
2026-06-27 23:31:52 +0200

    Update version and add menu to quit the app

--- KeyLit/MenuBarController.swift
+++ KeyLit/MenuBarController.swift
@@ -18,18 +18,37 @@ class MenuBarController: NSObject {
             // Link the click event directly to our action
             button.target = self
             button.action = #selector(statusBarButtonClicked(_:))
+            // Listen for both left and right mouse up events
+            button.sendAction(on: [.leftMouseUp, .rightMouseUp])
         }
     }
 
     @objc func statusBarButtonClicked(_ sender: NSStatusBarButton) {
-        isActivated.toggle()
+        guard let event = NSApp.currentEvent else { return }
         
-        let targetBrightness: Float = self.isActivated ? 0.0001 : 0.0
+        if event.type == .rightMouseUp || event.modifierFlags.contains(.control) {
+            showContextMenu()
+        } else {
+            isActivated.toggle()
+            
+            let targetBrightness: Float = self.isActivated ? 0.0001 : 0.0
+            
+            KeyboardBrightnessManager.shared.setBrightness(targetBrightness)
+            
+            sender.image = NSImage(systemSymbolName: "keyboard", accessibilityDescription: nil)
+            sender.alphaValue = isActivated ? 1.0 : 0.5
+        }
+    }
+    
+    private func showContextMenu() {
+        let menu = NSMenu()
+        let quitItem = NSMenuItem(title: "Quit KeyLit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
+        quitItem.target = NSApplication.shared
+        menu.addItem(quitItem)
         
-        KeyboardBrightnessManager.shared.setBrightness(targetBrightness)
-        
-        sender.image = NSImage(systemSymbolName: "keyboard", accessibilityDescription: nil)
-        sender.alphaValue = isActivated ? 1.0 : 0.5
+        if let statusItem = statusItem {
+            statusItem.popUpMenu(menu)
+        }
     }
     
     /// Reads the current brightness level from the manager to sync the UI
